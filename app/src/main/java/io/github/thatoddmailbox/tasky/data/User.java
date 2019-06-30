dd64f28b9b25c7799226bc4e13bce35bcc3f8aa4
Alex Studer <alex@studer.dev>
2019-06-30 15:33:42 -0400

    don't look for username or type since myhomeworkspace doesn't use those anymore

--- app/src/main/java/io/github/thatoddmailbox/tasky/data/User.java
+++ app/src/main/java/io/github/thatoddmailbox/tasky/data/User.java
@@ -6,18 +6,14 @@ import org.json.JSONObject;
 public class User {
     public int ID;
     public String Name;
-    public String Username;
     public String Email;
-    public String Type;
 
     public static User fromJSON(JSONObject o) throws JSONException {
         User u = new User();
 
         u.ID = o.getInt("id");
         u.Name = o.getString("name");
-        u.Username = o.getString("username");
         u.Email = o.getString("email");
-        u.Type = o.getString("type");
 
         return u;
     }
